e55e512f5a420981fdb067af53ca2a51b431c229
rokydas <droky136@gmail.com>
2021-12-10 19:35:43 +0600

    notification delete api done

--- backend/routes/notification.js
+++ backend/routes/notification.js
@@ -6,7 +6,7 @@ const verify = require('../verifyToken');
 const { addNotificationValidation } = require('../validation/notificationValidation')
 
 // get all notifications
-router.get('/all-notification', verify, async (req, res) => {
+router.get('/all', verify, async (req, res) => {
     const notifications = await Notification.find({})
     if (notifications) {
         res.status(200).send({
@@ -23,46 +23,46 @@ router.get('/all-notification', verify, async (req, res) => {
 
 // get a Notification
 router.get('/:id', verify, async (req, res) => {
-    const notification = await Notification.findOne({ _id: req.params.id })
-    if (notification) {
-        res.status(200).send({
-            success: true,
-            notification
-        })
-    } else {
-        res.status(404).json({
-            success: false,
-            msg: "Not found"
-        })
-    }
-})
-
-// post a Notification
-router.post('/add-notification', verify, async (req, res) => {
     if (mongoose.Types.ObjectId.isValid(req.params.id)) {
-        const newNotification = new Notification(req.body);
-        try {
-            const error = addNotificationValidation(req.body)
-            if (error) return res.status(400).send({
-                success: false,
-                msg: error.details[0].message
-            })
-
-            newNotification.save()
-            res.send({
+        const notification = await Notification.findOne({ _id: req.params.id })
+        if (notification) {
+            res.status(200).send({
                 success: true,
-                msg: "Notification added successfully"
+                notification
             })
-        } catch (err) {
-            res.status(400).send({
+        } else {
+            res.status(404).json({
                 success: false,
-                msg: "Something went wrong. Please try again"
+                msg: "Not found"
             })
         }
     } else {
         res.status(400).send({
             success: false,
             msg: "Invalid Id"
+        })
+    }
+})
+
+// post a Notification
+router.post('/add', verify, async (req, res) => {
+    const newNotification = new Notification(req.body);
+    try {
+        const error = addNotificationValidation(req.body)
+        if (error) return res.status(400).send({
+            success: false,
+            msg: error.details[0].message
+        })
+
+        newNotification.save()
+        res.send({
+            success: true,
+            msg: "Notification added successfully"
+        })
+    } catch (err) {
+        res.status(400).send({
+            success: false,
+            msg: "Something went wrong. Please try again"
         })
     }
 })
@@ -76,8 +76,13 @@ router.put('/update/:id', verify, async (req, res) => {
                 success: false,
                 msg: error.details[0].message
             })
-            const notification = await Notification.findByIdAndUpdate(req.params.id, { $set: req.body })
-            res.send(notification)
+            Notification.findByIdAndUpdate(req.params.id, { $set: req.body }, () => {
+                res.send({
+                    success: true,
+                    msg: "Notification updated successfully"
+                })
+            })
+
         } catch {
             res.status(400).send({
                 success: false,
@@ -92,9 +97,29 @@ router.put('/update/:id', verify, async (req, res) => {
     }
 })
 
-// post a Notification
-router.delete('/deleteNotification/:id', verify, async (req, res) => {
+// delete a Notification
+router.delete('/delete/:id', verify, async (req, res) => {
+    if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+        try {
+            Notification.findByIdAndDelete(req.params.id, () => {
+                res.send({
+                    success: true,
+                    msg: "Notification deleted successfully"
+                })
+            })
 
+        } catch {
+            res.status(400).send({
+                success: false,
+                msg: "Something went wrong. Please try again"
+            })
+        }
+    } else {
+        res.status(400).send({
+            success: false,
+            msg: "Invalid Id"
+        })
+    }
 })
 
 module.exports = router;
