ebb1278cefb143ddd0fd617517d5d669c405176e
rokydas <droky136@gmail.com>
2022-08-11 00:08:15 +0600

    description changed

--- frontend/src/components/HomeComponents/Header/Header.js
+++ frontend/src/components/HomeComponents/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
                 <div className="row">
                     <div className="col-md-6 p-5">
                         <h1>CSE CU Community</h1>
-                        <p>CSECU COMMUNITY is a web based system. The category of our system is online communities like as discussion group. The system will be help to benefit the current students of CSECU. Students can improve their skill by content sharing, seminar, workshop, etc by taking advice and contacting with teacher and expertise person from our department by the system.  They can also share innovative idea and development skill through the system. As a result, this approach will grow communication among students and teachers by sharing knowledge via blog and research paper. Students also can keep updated themselves about career opportunities in IT sector.</p>
+                        <p>CSECU COMMUNITY is a web based system. The category of our system is online communities like as discussion group. The system will be help to benefit the current students of CSECU. Students can improve their skill by content sharing, seminar, workshop, etc by taking advice and contacting with teacher and expertise person from our department by the system.</p>
                         <button className="custom-large-btn mb-2 me-3">Facebook Group</button>
                         <button className="blank-btn">Mail Us</button>
                     </div>
